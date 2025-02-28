68d09be172a831effaa7caf892cfb7b8fbfa6567
OnesmusBett <onesmuskipchumba5@gmail.com>
2025-02-28 10:45:04 +0300

    add pages routes

---
+++ app/ai/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AIPage() {
+  return (
+    <div>
+      AI analysis
+    </div>
+  )
+}
---
+++ app/upgrade/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Upgradepage() {
+  return (
+    <div>
+      Upgrade
+    </div>
+  )
+}
